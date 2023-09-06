5fdc866fd97c5acaeef856f8db0c77224f90dd8e
Goziechukwu <>
2023-09-06 18:31:39 +0000

    Declare constants for questions(included)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,160 @@
+// Declare the constants
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const questions = [
+    {
+        questionIndex: "Question One",
+        question: "Which is known for its use of indentation to define code blocks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Two",
+        question: "In which is Global Interpreter Lock (GIL) a potential limitation for multithreaded tasks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Three",
+        question: "Which is commonly used for building interactive web applications in browsers?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Four",
+        question: "Which is more prone to type coercion?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Five",
+        question: "Which has a stronger standard library with modules for various tasks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Six",
+        question: "Which relies on semicolons to terminate statements?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Seven",
+        question: "Which is dynamically typed but strongly typed?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Eight",
+        question: "Which language is widely used in scientific computing and machine learning?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Nine",
+        question: "Which language typically uses curly braces {} for defining code blocks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Ten",
+        question: "Which language uses async/await for handling asynchronous operations efficiently?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Eleven",
+        question: "Which language is known for its simplicity and readability?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twelve",
+        question: "Which language is more suitable for CPU-bound multithreaded tasks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Thirteen",
+        question: "Which language has a large community and ecosystem for both front-end and back-end development?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Fourteen",
+        question: "Which language is commonly used with libraries like NumPy and SciPy for scientific computing?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Fifteen",
+        question: "Which language has a standard library well-suited for web-related tasks like DOM manipulation?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Sixteen",
+        question: "Which language is frequently used for building RESTful APIs and server-side applications?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Seventeen",
+        question: "Which language has a strong emphasis on callback functions for handling asynchronous operations?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Eighteen",
+        question: "Which language has better performance optimization for web browsers?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Nineteen",
+        question: "Which language is preferred for machine learning and deep learning projects with libraries like TensorFlow?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twenty",
+        question: "Which language is often used for building web applications with frameworks like Django and Flask?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twenty-One",
+        question: "Which language's community is known for its rich ecosystem of packages and libraries available for various tasks?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twenty-Two",
+        question: "Which language is commonly used for data analysis and manipulation with libraries like Pandas?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twenty-three",
+        question: "Which language has a feature known as \"hoisting,\" where variable and function declarations are moved to the top of their containing scope during compilation?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    },
+    {
+        questionIndex: "Question Twenty-four",
+        question: "Which language is often associated with the use of decorators for modifying functions and methods at runtime?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "Python"
+    },
+    {
+        questionIndex: "Question Twenty-five",
+        question: "Which language uses the \"await\" keyword to pause the execution of a function until a promise is resolved?",
+        choices: ["JavaScript", "Python"],
+        correctChoice: "JavaScript"
+    }
+];
+
+
 
 // Set Function to Get the User's Filled-Out Form Details
 function getUserFormDetails(event) {
