b88aa277529a0a60cfe3da742f50cc125a992df1
Goziechukwu <>
2023-09-06 18:26:43 +0000

    Set outline of Quiz Functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+// Set Function to Get the User's Filled-Out Form Details
+function getUserFormDetails(event) {
+
+}
+
+// Set Functions to Reveal the Questions and to Set the Choices in Clickable Buttons
+//create the function to reveal the question: 
+function revealQuestion() {
+
+
+    // create buttons for the choices(options):
+    for() {
+
+        // create a click-handler function to select the choices by clicking:
+        function choiceClickHandler(choice) {
+
+
+        }
+    }
+}
+
+// Set Function to Check the Rightness or Wrongness of the Selected Choice and Alert the User
+function checkChoice(selectedChoice) {
+
+}
+
+// Set Function to End the Quiz and Display the User's Final Score
+function endQuiz() {
+
+}
+
+revealQuestion();
